c9b36a836789cff0f8236bce7e5139d0587a3a71
Jan Erlebach <erlebachj24@spstrutnov.cz>
2026-03-03 11:33:51 +0100

    Pridani Loginu - zatim jen sablona a navigace pomoci URL
    + samostatny controller pro Usera

---
+++ ASPNETCore/ASPNETCore/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCore.Controllers;
+
+public class UserController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return RedirectToAction("Index", "Home");
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public IActionResult Profile()
+    {
+        return View();
+    }
+}
