bd2ee4dba9f4cffd1593dbb91512b29337e72efc
Jan Erlebach <erlebachj24@spstrutnov.cz>
2026-03-09 16:14:42 +0100

    Odkazy na tlacitkach Login a Register
    Varovani u psani hesel a zapomenuti username u Register - vse v kontroleru

--- ASPNETCore/ASPNETCore/Controllers/UserController.cs
+++ ASPNETCore/ASPNETCore/Controllers/UserController.cs
@@ -15,11 +15,29 @@ public class UserController : Controller
         return View();
     }
 
+    [HttpGet]
     public IActionResult Register()
     {
         return View();
     }
-
+    [HttpPost]
+    public IActionResult Register(string username, string password, string passwordCheck)
+    {
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+        {
+            ViewData["chyba"] = "Jméno nebo heslo není zadáno.";
+            return View();
+        }
+        else if (password != passwordCheck) 
+        {
+            ViewData["chyba"] = "Hesla se neshodují.";
+            return View();
+        }
+        else
+        {
+            return Redirect("/User/Login");
+        }
+    }
     public IActionResult Profile()
     {
         return View();
