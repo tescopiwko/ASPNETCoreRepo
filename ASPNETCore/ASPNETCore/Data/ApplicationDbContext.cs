7bd4c03f78899463e858ddb4c9bfdd2355144141
Jan Erlebach <erlebachj24@spstrutnov.cz>
2026-03-09 16:45:06 +0100

    Vytvoreni databaze
    konvence v appsettings.json
    konstruktor v ApplicationDbContext.cs
    konvence a usingy v Program.cs

---
+++ ASPNETCore/ASPNETCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ASPNETCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- ASPNETCore/ASPNETCore/Program.cs
+++ ASPNETCore/ASPNETCore/Program.cs
@@ -1,3 +1,6 @@
+using ASPNETCore.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ASPNETCore
 {
     public class Program
@@ -8,6 +11,9 @@ namespace ASPNETCore
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
