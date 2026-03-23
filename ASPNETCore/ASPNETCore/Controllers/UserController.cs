598c932bbf33e1df63ccb73a80a8ae775ae00ffa
Jan Erlebach <erlebachj24@spstrutnov.cz>
2026-03-23 14:57:39 +0100

    Logika prihlaseni uzivatele

--- ASPNETCore/ASPNETCore/Controllers/UserController.cs
+++ ASPNETCore/ASPNETCore/Controllers/UserController.cs
@@ -21,10 +21,6 @@ public class UserController : Controller
         return RedirectToAction("Index", "Home");
     }
 
-    public IActionResult Login()
-    {
-        return View();
-    }
 
     [HttpGet]
     public IActionResult Register()
@@ -39,25 +35,65 @@ public class UserController : Controller
             ViewData["chyba"] = "Jméno nebo heslo není zadáno.";
             return View();
         }
-        else if (password != passwordCheck) 
+        if (password != passwordCheck) 
         {
             ViewData["chyba"] = "Hesla se neshodují.";
             return View();
         }
-        else
+       
+        
+        _db.Users.Add(new User()
         {
-            _db.Users.Add(new User()
-            {
-                Username = username,
-                Password = password
-            });
-            _db.SaveChanges();
+            Username = username,
+            Password = password
+        });
+        _db.SaveChanges();
 
-            return Redirect("/User/Login");
-        }
+        return Redirect("/User/Login");
+        
     }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        if (string.IsNullOrEmpty(username) ||
+            string.IsNullOrEmpty(password))
+        {
+            ViewData["chyba"] = "Jméno nebo heslo není zadáno.";
+
+            return View();
+        }
+
+        User? loggedUser = _db
+            .Users
+            .Where(u => u.Username == username)
+            .FirstOrDefault();
+
+        if (loggedUser == null)
+        {
+            ViewData["chyba"] = "Uživatel nenalezen.";
+            return View();
+        }
+
+        if (loggedUser.Password != password)
+        {
+            ViewData["chyba"] = "Neplatné heslo.";
+            return View();
+        }
+
+        return Redirect("/User/Profile");
+    }
+
     public IActionResult Profile()
     {
         return View();
     }
+
+
 }
